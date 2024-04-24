e07f3258dad974c8b0c981e9ac82a3f048a746ce
Frankincense <160001436+GoldFrankincenseMyrrh@users.noreply.github.com>
2024-04-24 13:36:15 +0800

    Add files via upload

---
+++ CourseCurriculum.java
@@ -0,0 +1,10 @@
+package FinalGroupProject;
+
+public abstract class CourseCurriculum {
+    private String courseNumber;
+    private String descriptiveTitle;
+    private byte units;
+    private byte term;
+    private byte yearLevel;
+    private double grade;
+}
